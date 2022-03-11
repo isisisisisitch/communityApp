767b4c8f56edc3663971d74e49308a2b63e466e1
isisisisisisithch <dallucus@gmail.com>
2022-03-11 12:20:42 -0500

    1.added getShopInfo in shopoperation.js 2.debugged getShopById and modifyShop both front end and back end(http://localhost:8080/communityApp/shopadmin/shopoperation?shopId=1)

--- src/main/webapp/resources/js/shop/shopoperation.js
+++ src/main/webapp/resources/js/shop/shopoperation.js
@@ -3,12 +3,62 @@
  */
 $(function() {
 
+	// 从URL里获取shopId参数的值
+	var shopId = getQueryString('shopId');
+
+	// 由于店铺注册和编辑使用的是同一个页面，
+	// 该标识符用来标明本次是添加还是编辑操作
+	var isEdit = shopId ? true : false;
+
 	// 用于店铺注册时候的店铺类别以及区域列表的初始化的URL
 	var initUrl = '/communityApp/shopadmin/getshopinitinfo';
+
 	// 注册店铺的URL
 	var registerShopUrl = '/communityApp/shopadmin/registershop';
-	//alert(initUrl);
-	getShopInitInfo();
+
+	// 编辑店铺前需要获取店铺信息，这里为获取当前店铺信息的URL
+	var shopInfoUrl = "/communityApp/shopadmin/getshopbyid?shopId=" + shopId;
+
+	// 编辑店铺信息的URL
+	var editShopUrl = '/communityApp/shopadmin/modifyshop';
+
+	if (!isEdit) {
+		getShopInitInfo();
+	} else {
+		getShopInfo(shopId);
+	}
+
+	// 通过店铺Id获取店铺信息
+	function getShopInfo(shopId) {
+		$.getJSON(shopInfoUrl, function(data) {
+			if (data.success) {
+				// 若访问成功，则依据后台传递过来的店铺信息为表单元素赋值
+				var shop = data.shop;
+				$('#shop-name').val(shop.shopName);
+				$('#shop-addr').val(shop.shopAddr);
+				$('#shop-phone').val(shop.phone);
+				$('#shop-desc').val(shop.shopDesc);
+				// 给店铺类别选定原先的店铺类别值
+				var shopCategory = '<option data-id="'
+					+ shop.shopCategory.shopCategoryId + '" selected>'
+					+ shop.shopCategory.shopCategoryName + '</option>';
+				var tempAreaHtml = '';
+				// 初始化区域列表
+				data.areaList.map(function(item, index) {
+					tempAreaHtml += '<option data-id="' + item.areaId + '">'
+						+ item.areaName + '</option>';
+				});
+				$('#shop-category').html(shopCategory);
+				// 不允许选择店铺类别
+				$('#shop-category').attr('disabled', 'disabled');
+				$('#area').html(tempAreaHtml);
+				// 给店铺选定原先的所属的区域
+				$("#area option[data-id='" + shop.area.areaId + "']").attr(
+					"selected", "selected");
+			}
+		});
+	}
+
 
 	// 取得所有二级店铺类别以及区域信息，并分别赋值进类别列表以及区域列表
 	function getShopInitInfo() {
@@ -33,6 +83,12 @@ $(function() {
 	$('#submit').click(function() {
 		// 创建shop对象
 		var shop = {};
+
+		if (isEdit) {
+			// 若属于编辑，则给shopId赋值
+			shop.shopId = shopId;
+		}
+
 		// 获取表单里的数据并填充进对应的店铺属性中
 		shop.shopName = $('#shop-name').val();
 		shop.shopAddr = $('#shop-addr').val();
@@ -69,7 +125,7 @@ $(function() {
 
 		// 将数据提交至后台处理相关操作
 		$.ajax({
-			url : registerShopUrl,
+			url : (isEdit ? editShopUrl : registerShopUrl),
 			type : 'POST',
 			data : formData,
 			contentType : false,
