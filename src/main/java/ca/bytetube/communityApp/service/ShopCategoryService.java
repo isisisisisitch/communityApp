62552b1c468f39d8f478bac1eebdd02ac69b426e
isisisisisisithch <dallucus@gmail.com>
2022-04-06 17:28:53 -0400

    1.updated HeadlineServiceImpl and shopCategoryServiceImpl with redis

--- src/main/java/ca/bytetube/communityApp/service/ShopCategoryService.java
+++ src/main/java/ca/bytetube/communityApp/service/ShopCategoryService.java
@@ -5,7 +5,7 @@ import ca.bytetube.communityApp.entity.ShopCategory;
 import java.util.List;
 
 public interface ShopCategoryService {
-
+	public static final String SCLISTKEY = "shopcategorylist";
 	/**
 	 * 根据查询条件获取ShopCategory列表
 	 * @param shopCategoryCondition
