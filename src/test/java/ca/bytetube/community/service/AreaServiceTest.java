c98e2734313b40fed1ad5e4cba9a3a5a39d2f502
isisisisisisithch <dallucus@gmail.com>
2022-04-06 17:42:06 -0400

    1.added cache remove

--- src/test/java/ca/bytetube/community/service/AreaServiceTest.java
+++ src/test/java/ca/bytetube/community/service/AreaServiceTest.java
@@ -3,6 +3,7 @@ package ca.bytetube.community.service;
 import ca.bytetube.community.BaseTest;
 import ca.bytetube.communityApp.entity.Area;
 import ca.bytetube.communityApp.service.AreaService;
+import ca.bytetube.communityApp.service.CacheService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,12 +14,14 @@ import static org.junit.Assert.assertEquals;
 public class AreaServiceTest extends BaseTest {
 	@Autowired
 	private AreaService areaService;
-
+	@Autowired
+	private CacheService cacheService;
 	@Test
 	public void testGetAreaList() {
 		List<Area> areaList = areaService.getAreaList();
 		System.out.println(areaList.get(0).getAreaName());
 		assertEquals("montreal", areaList.get(0).getAreaName());
-
+		cacheService.removeFromCache(areaService.AREALISTKEY);
+		areaList = areaService.getAreaList();
 	}
 }
